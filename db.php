2fbc1b89f67a887b59eaf3f8692f22b3644017eb
p3xcnrtm <essyenpeter57@gmail.com>
2025-04-30 15:49:52 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("v8d.h.filess.io","QfsLedger_fieldzulu","46557c4ef79234b6221acecd840ea63f1f9c8534","QfsLedger_fieldzulu","3306");
+$con = mysqli_connect("o6wkg.h.filess.io","teamwealth_becausemad","00ad00f565a6be51fb2e8914270b2999eb31a598","teamwealth_becausemad","61001");
 //$con = mysqli_connect("sql7.freemysqlhosting.net","sql7748837","L2CSaDcTLK","sql7748837","3306");
 // Check connection
 if (mysqli_connect_errno())
